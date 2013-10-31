670530ab0c49156cf70e2cc0ef3ff5f591ccfdc7
Oran <oran@win.com>
2013-10-31 19:30:09 +0200

    playing with the code

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 #
 class Logger( object ):
@@ -33,27 +34,58 @@ class Logger( object ):
 
     #
     @classmethod
-    def before( self, level, decorator_arg1, decorator_arg2 ):
-
-        print "I make decorators! And I accept arguments:", decorator_arg1, decorator_arg2
+    def before( cls, level, string ):
+        print "I make decorators! And I accept arguments:", string
+        attrs = cls._get_unique_params( string )
+        print attrs
+        print "\nPRINT ARGS:"
+        for attr, prefix_str in attrs:
+            if re.match( "({\d+})", attr ):
+                cls._print_param( attr, prefix_str )
 
         def my_decorator( func ):
             # The ability to pass arguments here is a gift from closures.
             # If you are not comfortable with closures, you can assume it's ok,
             # or read: http://stackoverflow.com/questions/13857/can-you-explain-closures-as-they-relate-to-python
-            print "I am the decorator. Somehow you passed me arguments:", decorator_arg1, decorator_arg2
+            
+            # print "I am the decorator. Somehow you passed me arguments:", string
 
             # Don't confuse decorator arguments and function arguments!
-            def wrapped( function_arg1, function_arg2 ) :
-                print ( "I am the wrapper around the decorated function.\n"
-                      "I can access all the variables\n"
-                      "\t- from the decorator: {0} {1}\n"
-                      "\t- from the function call: {2} {3}\n"
-                      "Then I can pass them to the decorated function"
-                      .format( decorator_arg1, decorator_arg2,
-                              function_arg1, function_arg2 ) )
-                return func( function_arg1, function_arg2 )
+            def wrapped_all( *args, **kwargs ) :
+                #print ( "I am the wrapper around the decorated function.\n"
+                #      "I can access all the variables\n"
+                #      "\t- from the decorator: {0}\n"
+                #      "\t- from the function call: {1} {2}\n"
+                #      "Then I can pass them to the decorated function"
+                #      .format( string, args, kwargs ) )
+                #
+                return func( args, kwargs )
 
-            return wrapped
+            return wrapped_all
 
         return my_decorator
+
+    #
+    @classmethod
+    def _get_unique_params( cls, string ):
+        """
+        get a list of tuples with:
+            1.  Special, well known, paramater.
+            2.  String untill the parameter position.
+        """
+        #print re.findall( "({\d+})", "first arg {0}second group {1}" )
+        start_index = 0
+        attr_tuples = []
+        
+        attrs = re.findall( "({\d+})", string )
+        for attr in attrs:
+            curr_index = string.find( attr )
+            attr_tuples.append( ( attr, string[ start_index : curr_index ] ) )
+            start_index += curr_index + len( attr )
+
+        return attr_tuples
+
+    #
+    @classmethod
+    def _print_param( cls, attr, prefix_str ):
+        print prefix_str, attr # instead of attr we will print *args[ in position ]
