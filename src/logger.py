a306a494d5d5606d8dc460e53ecfdfb61a31b733
Oran <oran@win.com>
2013-10-31 15:10:08 +0200

    playing with the before method

--- src/logger.py
+++ src/logger.py
@@ -6,9 +6,7 @@ class Logger( object ):
     Advanced Logger using decorator to log ops before and after methods 
     """
 
-    #
-    def __init__( self ):
-        self._logger = None
+    _logger = None
 
     @property
     def logger( self ):
@@ -32,3 +30,30 @@ class Logger( object ):
     #
     def _default_logger( self ):
         return logging.getLogger()
+
+    #
+    @classmethod
+    def before( self, level, decorator_arg1, decorator_arg2 ):
+
+        print "I make decorators! And I accept arguments:", decorator_arg1, decorator_arg2
+
+        def my_decorator( func ):
+            # The ability to pass arguments here is a gift from closures.
+            # If you are not comfortable with closures, you can assume it's ok,
+            # or read: http://stackoverflow.com/questions/13857/can-you-explain-closures-as-they-relate-to-python
+            print "I am the decorator. Somehow you passed me arguments:", decorator_arg1, decorator_arg2
+
+            # Don't confuse decorator arguments and function arguments!
+            def wrapped( function_arg1, function_arg2 ) :
+                print ( "I am the wrapper around the decorated function.\n"
+                      "I can access all the variables\n"
+                      "\t- from the decorator: {0} {1}\n"
+                      "\t- from the function call: {2} {3}\n"
+                      "Then I can pass them to the decorated function"
+                      .format( decorator_arg1, decorator_arg2,
+                              function_arg1, function_arg2 ) )
+                return func( function_arg1, function_arg2 )
+
+            return wrapped
+
+        return my_decorator
