73a11f6e96fccd03059f16b4fe64ebfa68fd2b36
Oran Ben-Zur <oran@broadmann.com>
2020-04-06 08:59:18 +0300

    update

--- src/logger.py
+++ src/logger.py
@@ -2,90 +2,84 @@ import logging
 import re
 
 #
-class Logger( object ):
-    """ 
-    Advanced Logger using decorator to log ops before and after methods 
+class Logger:
     """
-
+    Advanced Logger using decorator to log ops before and after methods
+    """
     _logger = None
 
     @property
-    def logger( self ):
-        """ 
+    def logger(self):
+        """
         Gets the logger property, if the .with_logger() method was invoked,
         Otherwise raise an Exception.
         """
         if self._logger is not None:
             return self._logger
         # TODO: create my own exceptions, inhereted from class Exception
-        raise Exception( "Invoke `.with_logger()` to set your log" )
+        raise Exception('Invoke `.with_logger()` to set your log')
 
-    #
-    def with_logger( self, log = None ):
-        """ Setter for the logger property, use None for default """
-        if not ( log is None or isinstance( log, logging.Logger ) ):
-            raise ValueError( "`log` must be a Logger object or None" )
+    def with_logger(self, log=None):
+        """Setter for the logger property, use None for default."""
+        if not (log is None or isinstance(log, logging.Logger)):
+            raise ValueError('`log` must be a Logger object or None')
 
         self._logger = log if log is not None else self._default_logger()
 
-    #
-    def _default_logger( self ):
+    def _default_logger(self):
         return logging.getLogger()
 
-    #
     @classmethod
-    def before( cls, level, string ):
-        print "I make decorators! And I accept arguments:", string
-        attrs = cls._get_unique_params( string )
-        print attrs
-        print "\nPRINT ARGS:"
+    def before(cls, level, string):
+        print('I make decorators! And I accept arguments:', string)
+        attrs = cls._get_unique_params(string)
+        print(attrs)
+        print('\nprint( ARGS:')
         for attr, prefix_str in attrs:
-            if re.match( "({\d+})", attr ):
-                cls._print_param( attr, prefix_str )
+            if re.match('({\d+})', attr):
+                cls._print_param(attr, prefix_str)
 
-        def my_decorator( func ):
+        def my_decorator(func):
             # The ability to pass arguments here is a gift from closures.
             # If you are not comfortable with closures, you can assume it's ok,
             # or read: http://stackoverflow.com/questions/13857/can-you-explain-closures-as-they-relate-to-python
-            
-            # print "I am the decorator. Somehow you passed me arguments:", string
+
+            # print('I am the decorator. Somehow you passed me arguments:', string)
 
             # Don't confuse decorator arguments and function arguments!
-            def wrapped_all( *args, **kwargs ) :
-                #print ( "I am the wrapper around the decorated function.\n"
-                #      "I can access all the variables\n"
-                #      "\t- from the decorator: {0}\n"
-                #      "\t- from the function call: {1} {2}\n"
-                #      "Then I can pass them to the decorated function"
-                #      .format( string, args, kwargs ) )
+            def wrapped_all(*args, **kwargs):
+                #print('I am the wrapper around the decorated function.\n'
+                #      'I can access all the variables\n'
+                #      '\t- from the decorator: {0}\n'
+                #      '\t- from the function call: {1} {2}\n'
+                #      'Then I can pass them to the decorated function'
+                #      .format(string, args, kwargs))
                 #
-                return func( args, kwargs )
+                return func(args, kwargs)
 
             return wrapped_all
 
         return my_decorator
 
-    #
     @classmethod
-    def _get_unique_params( cls, string ):
+    def _get_unique_params(cls, string):
         """
-        get a list of tuples with:
+        Get a list of tuples with:
             1.  Special, well known, paramater.
             2.  String untill the parameter position.
         """
-        #print re.findall( "({\d+})", "first arg {0}second group {1}" )
+        #print(re.findall('({\d+})', 'first arg {0}second group {1}'))
         start_index = 0
         attr_tuples = []
-        
-        attrs = re.findall( "({\d+})", string )
+
+        attrs = re.findall('({\d+})', string)
         for attr in attrs:
-            curr_index = string.find( attr )
-            attr_tuples.append( ( attr, string[ start_index : curr_index ] ) )
-            start_index += curr_index + len( attr )
+            curr_index = string.find(attr)
+            attr_tuples.append((attr, string[start_index : curr_index]))
+            start_index += curr_index + len(attr)
 
         return attr_tuples
 
-    #
     @classmethod
-    def _print_param( cls, attr, prefix_str ):
-        print prefix_str, attr # instead of attr we will print *args[ in position ]
+    def _print_param(cls, attr, prefix_str):
+        print(prefix_str, attr) # instead of attr we will print(*args[in position])
--- src/logger_test.py
+++ src/logger_test.py
@@ -3,33 +3,27 @@ import logging
 
 from logger import Logger
 
-##
-class TestLogger( unittest.TestCase ):
 
-	#
-	def test_logger_not_set( self ):
-		""" A logger object was not set """
+class TestLogger(unittest.TestCase):
 
+	def test_logger_not_set(self):
+		"""A logger object was not set"""
 		# Assert
-		self.assertRaises( Exception, lambda: Logger().logger )
+		self.assertRaises(Exception, lambda: Logger().logger)
 
-	#
-	def test_logger_set( self ):
-		""" A logger object was set """
-
+	def test_logger_set(self):
+		"""A logger object was set"""
 		# Arrange
 		log = Logger()
 
 		# Act
-		log._logger = logging.Logger( "test" )
+		log._logger = logging.Logger('test')
 
 		# Assert
-		self.assertIsNotNone( log.logger )
+		self.assertIsNotNone(log.logger)
 
-	#
-	def test_with_logger_default( self ):
-		""" Set a default logger """
-
+	def test_with_logger_default(self):
+		"""Set a default logger"""
 		# Arrange
 		log = Logger()
 		expected = log._default_logger()
@@ -38,46 +32,39 @@ class TestLogger( unittest.TestCase ):
 		log.with_logger()
 
 		# Assert
-		self.assertEqual( expected, log.logger )
+		self.assertEqual(expected, log.logger)
 
-	#
-	def test_with_logger_wrong_obj( self ):
-		""" Set a None logger object """
-
+	def test_with_logger_wrong_obj(self):
+		"""Set a None logger object"""
 		# Assert
-		self.assertRaises( ValueError, Logger().with_logger, "test" )
+		self.assertRaises(ValueError, Logger().with_logger, 'test')
 
-	#
-	def test_with_logger( self ):
-		""" Set a logger object """
-
+	def test_with_logger(self):
+		"""Set a logger object"""
 		# Arrange
 		log = Logger()
 
 		# Act
-		log.with_logger( logging.getLogger() )
+		log.with_logger(logging.getLogger())
 
 		# Assert
-		self.assertIsNotNone( log.logger )
+		self.assertIsNotNone(log.logger)
 
-	#
-	def test_before( self ):
-
+	def test_before(self):
 		# Arrange
 		log = Logger()
 
 		# Act
-		log.with_logger( logging.getLogger() )
+		log.with_logger(logging.getLogger())
+
+		@log.before(logging.INFO, 'First param: {0}\nSecond param: {1}')
+		def wrapper(function_arg1, function_arg2):
+		    print('Done')
+            #print( ('I am the decorated function and only knows about my arguments: {0}'
+		    #       ' {1}'.format(function_arg1, function_arg2))
+
+		wrapper('Rajesh', 'Howard')
 
 
-		@log.before( logging.INFO, "First param: {0}\nSecond param: {1}" )
-		def wrapper( function_arg1, function_arg2 ):
-		    print "Done"
-            #print ( "I am the decorated function and only knows about my arguments: {0}"
-		    #       " {1}".format( function_arg1, function_arg2 ) )
-
-		wrapper( "Rajesh", "Howard" )
-	
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 	unittest.main()
